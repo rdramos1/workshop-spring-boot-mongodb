a2d06e95dd62203c2b0a2107605e116b566ea8aa
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-10-05 19:16:26 -0300

    Consulta simples com query methods

--- src/main/java/com/ramos/workshopmongo/service/PostService.java
+++ src/main/java/com/ramos/workshopmongo/service/PostService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class PostService {
@@ -20,4 +21,9 @@ public class PostService {
     public Post findById(String id) {
         return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
+
 }
