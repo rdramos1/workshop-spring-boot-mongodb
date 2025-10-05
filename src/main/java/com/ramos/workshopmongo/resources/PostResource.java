991f7558d3736df5e1f3628ed5e366829ad697d7
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-10-05 19:16:26 -0300

    Consulta simples com @Query

--- src/main/java/com/ramos/workshopmongo/resources/PostResource.java
+++ src/main/java/com/ramos/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.ramos.workshopmongo.resources;
 import com.ramos.workshopmongo.domain.Post;
 import com.ramos.workshopmongo.domain.User;
 import com.ramos.workshopmongo.dto.UserDTO;
+import com.ramos.workshopmongo.resources.util.URL;
 import com.ramos.workshopmongo.service.PostService;
 import com.ramos.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
